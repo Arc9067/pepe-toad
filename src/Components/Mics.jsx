ca4eebc5c3d778b91e2fa528144c3de358de1586
Arc <atandarokib@gmail.com>
2023-07-13 21:39:11 +0100

    up

--- src/Components/Mics.jsx
+++ src/Components/Mics.jsx
@@ -9,7 +9,7 @@ const Mics = () => {
       <div className="container grid grid-cols-1 lg:grid-cols-2 ">
         <div className=""> </div>
         <article className="flex flex-col gap-9">
-          <h1 className="font-special text-7xl text-amber-700">Tekenomics</h1>
+          <h1 className="font-special text-7xl text-amber-700">Tokenomics</h1>
           <div className="font-special flex-col justify-start items-start gap-[33px] inline-flex">
             <div className="text-white text-[32px] font-normal">
               Total Supply: 1, 000, 000
